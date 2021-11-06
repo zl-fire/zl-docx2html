d51618a54003011054aa96c5634fdea5491878b8
zhangluzhanglu <1758658975@qq.com>
2021-11-06 15:38:21 +0800

    调整菜单生成序号时的样式

--- src/module/addHsOrder.js
+++ src/module/addHsOrder.js
@@ -9,42 +9,51 @@ function addHsOrder($, list) {
     // 然后在遍历所有标题，依次将序号写入进去
     let hs = $("h1,h2,h3,h4,h5,h6");
     for (let i = 0; i < hs.length; i++) {
-        let tagName, text;
+        // let tagName, text;
+        let text;
         try {
             windows;
-            tagName = hs[i].tagName;
+            // tagName = hs[i].tagName;
             text = hs[i].innerText;
         }
         catch {
-            tagName = hs[i].name;
+            // tagName = hs[i].name;
             text = $(hs[i]).text();
 
         }
         let eleId = $(hs[i]).prop("id");
         // 获取新的名字：序号+原名
-        let newName = handOrder({ tagName, text, id2HsMap, eleId });
+        // let newName = handOrder({ tagName, text, id2HsMap, eleId });
+        let newName =  id2HsMap[eleId]+". "+text;
         //向元素注入新的，名字
         $(hs[i]).text(newName);
     }
     return $.html();
 }
 
-function deepCall(children, id2HsMap) {
+function deepCall(children, id2HsMap, parOrder) {
     if (children && children.length > 0) {
         for (let i = 0; i < children.length; i++) {
-            id2HsMap[children[i].eleId] = i + 1;
             // 修改标题（加上序号）
-            let tagName = children[i].tag;
             let text = children[i].name;
-            let orderNum = i + 1;
-            let newName = handOrder({ tagName, text, orderNum });
+            let orderNum;
+            if (parOrder) {
+                orderNum = parOrder + "." + (i + 1);
+            }
+            else {
+                orderNum = i + 1;
+            }
+            id2HsMap[children[i].eleId] = orderNum;
+            // let newName = handOrder({ tagName, text, orderNum }); //不再使用这个序号编码，
+            let newName = orderNum + ". " + text; //新序号方式全部使用阿拉伯数字
             children[i].name = newName
             //递归调用
-            deepCall(children[i].children, id2HsMap);
+            deepCall(children[i].children, id2HsMap, orderNum);
         }
     }
 }
 
+// 新的序号方式全部变为阿拉伯数字，即下面的方法不再使用
 function handOrder({ tagName, text, id2HsMap, eleId, orderNum }) {
     // 获取新的名字：序号+原名
     if (!orderNum) orderNum = id2HsMap[eleId];
@@ -62,10 +71,10 @@ function handOrder({ tagName, text, id2HsMap, eleId, orderNum }) {
         newName = "(" + orderNum + "). " + text;//格式:(1). 
     }
     else if (tagName == "h5" || tagName == "H5") {
-        newName = numToEng(orderNum, true)  + ". " + text;//格式:A. 
+        newName = numToEng(orderNum, true) + ". " + text;//格式:A. 
     }
     else {
-        newName = numToEng(orderNum)  + ". " + text;//格式:a.
+        newName = numToEng(orderNum) + ". " + text;//格式:a.
     }
     return newName;
 }
--- test.js
+++ test.js
@@ -1,22 +1,27 @@
 var { batchDocx2html, docx2html } = require("./index.js");
-// (async function () {
-//     console.log("转换完成1");
-//     await docx2html({
-//         docxPath: "./1. vue一些补充知识点.docx",
-//         showWarnMessage: false,
-//     });
-//     console.log("转换完成2");
-// })()
 
-batchDocx2html({
-    // dirPath: "./doctest",
-    dirPath: "../",
-    outPath:"./HTML",
-    showWarnMessage: false,
-    showExeResult:true,
-    isList2file:true,
-    list2filePath:"./aa/bb/cc/dd/tt/tree.json"
-});
+// 单个转换
+(async function () {
+    console.log("转换完成1");
+    await docx2html({
+        docxPath: "./nginx基本使用.docx",
+        showWarnMessage: false,
+    });
+    console.log("转换完成2");
+})()
+
+
+
+// 批量转换
+// batchDocx2html({
+//     // dirPath: "./doctest",
+//     dirPath: "../",
+//     outPath:"./HTML",
+//     showWarnMessage: false,
+//     showExeResult:true,
+//     isList2file:true,
+//     list2filePath:"./aa/bb/cc/dd/tt/tree.json"
+// });
 
 // let extname = path.extname("1. vue一些补充知识点.docx");
 
