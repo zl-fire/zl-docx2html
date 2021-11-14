b24bcea24b8d3bafa2fc953846a6ac927c86d8d1
zhangluzhanglu <1758658975@qq.com>
2021-11-14 23:21:31 +0800

    打包发版

--- index.js
+++ index.js
@@ -100,7 +100,7 @@
         }
         if (docType !== "docx") {
             return `<!DOCTYPE html>
-        <html>
+        <html lang="zh-cn">
             <head>
             <meta charset="utf-8" >
             <meta name="viewport" content="width=device-width, initial-scale=1">
